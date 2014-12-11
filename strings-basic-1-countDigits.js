01e24c4b29fd2b165156843d2ce38979137e9d51
Brandon Foster <brandon.foster.vt@gmail.com>
2014-12-11 13:01:43 -0500

    strings basic 1: update description

--- strings-basic-1-countDigits.js
+++ strings-basic-1-countDigits.js
@@ -1,5 +1,8 @@
 /*
- * Given a string, find the sum of all the digits in the string.
+    Given an ASCII string, find the sum of all the numeric characters in the 
+    string. For example, for 'abc1234z' the sum of all numeric characters is. 
+    What is the sum of all the numeric characters in the string below?
+    '96h11k4959q615948s50922o38h1453ij38w73413d5577lzrqw3780b389750vf100zd29z73j5wh73l6965n85vm77cw10awrjr29265289222238n10013uk10062f9449acbhfgcm35j78q80'
  */
 
 function isNumber(char) {
